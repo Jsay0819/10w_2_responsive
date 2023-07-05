a5ac5fa0463b9b86c88390d93ecee9d9ee63e6aa
jsay <jsxhxx0819@gmail.com>
2023-07-05 16:13:32 +0900

    230705 수업

--- js/flat.js
+++ js/flat.js
@@ -1,14 +1,19 @@
-window.onload = function () {
-  var menuToggleBtn = document.querySelector(".menu_toggle_btn");
-  var gnb = document.querySelector(".gnb");
+// window.onload = function () {
+//   var menuToggleBtn = document.querySelector(".menu_toggle_btn");
+//   var gnb = document.querySelector(".gnb");
 
-  menuToggleBtn.addEventListener("click", function () {
-    // if (gnb.style.display === "none") {
-    //   gnb.style.display = "block";
-    // } else {
-    //   gnb.style.display = "none";
-    // }
-    gnb.style.display = gnb.style.display === "none" ? "block" : "none";
-    //   위에꺼 보다 코드 훨씬 간단해짐
+//   menuToggleBtn.addEventListener("click", function () {
+//     // if (gnb.style.display === "none") {
+//     //   gnb.style.display = "block";
+//     // } else {
+//     //   gnb.style.display = "none";
+//     // }
+//     gnb.style.display = gnb.style.display === "none" ? "block" : "none";
+//     //   위에꺼 보다 코드 훨씬 간단해짐
+//   });
+// };
+jQuery(function ($) {
+  $(".menu_toggle_btn").click(function () {
+    $(".gnb").stop().slideToggle("fast");
   });
-};
+});
